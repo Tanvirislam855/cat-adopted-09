7154ecaf31a6ddc260da301b7103825721455c1e
Tanvirislam855e <ajtanvir855@gmail.com>
2026-05-22 21:00:44 +0600

    image addad

--- src/app/all-pets/page.jsx
+++ src/app/all-pets/page.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import AllPetsCard from '@/components/all-petsCard.jsx';
 const AllPetsPage = async () => {
     const res = await fetch('http://localhost:8000/all-pets')
     const allPets = await res.json()
@@ -7,10 +8,10 @@ const AllPetsPage = async () => {
     return (
         <div>
             <h1>All Pets</h1>
-            <div>
+            <div className='grid grid-cols-6 gap-5'>
                 {
-                    allPets.map(allPets => <div key={allPets._id}>
-                        {allPets.allPetsName} </div>)
+                    allPets.map(pet => <AllPetsCard key={pet._id} allpetsCard={pet} />)
+                        
                 }
             </div>
         </div>
--- src/components/all-petsCard.jsx
+++ src/components/all-petsCard.jsx
@@ -1,8 +1,33 @@
-import React from 'react';
-
-const allpetsCard = () => {
+import Image from "next/image";
+import { LuMapPin } from "react-icons/lu";
+import { FaRegCalendar } from "react-icons/fa";
+const allpetsCard = ({allpetsCard}) => {
+    const {imageUrl,location,gender,age,petName,adoptionfee} = allpetsCard
     return (
-        <div>
+        <div className="border">
+            <Image
+           alt={petName}
+           src={imageUrl}
+           height={400}
+           width={400}
+            />
+            <div className="p-2">
+        <div className="flex items-center gap-1">
+          <LuMapPin /> <span>{location}</span>
+        </div>
+        <div className="flex justify-between">
+          <div>
+            <div>
+              <h2 className="text-xl font-bold">{petName}</h2>
+            </div>
+            <div className="flex gap-1 items-center">
+              <FaRegCalendar /> {age}
+            </div>
+          
+          </div>
+          </div>
+          </div>
+
             
         </div>
     );
